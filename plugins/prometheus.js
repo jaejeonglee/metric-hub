56fe6034ba4f71b1f6b229e97d8b8692569a26ba
jaejeonglee <ljj5256@gmail.com>
2025-10-28 14:16:28 +0900

    feat: prometheus plugin

---
+++ plugins/prometheus.js
@@ -0,0 +1,45 @@
+import fp from "fastify-plugin";
+import { readFile, writeFile } from "fs/promises";
+
+async function prometheusPlugin(fastify, options) {
+  const { config } = options;
+  const targetsFilePath = config.prometheus.targetsFile;
+
+  /*
+   * Decorate "prometheus"
+   */
+  fastify.decorate("prometheus", {
+    addTarget,
+  });
+
+  /*
+   * Add target to Prometheus targets file
+   */
+  async function addTarget(hostname, ip, port) {
+    let targetsJson = [];
+    try {
+      const targetsFile = await readFile(targetsFilePath, "utf-8");
+      targetsJson = JSON.parse(targetsFile);
+    } catch (readErr) {
+      fastify.log.warn(`Could not read ${targetsFilePath}, starting new list.`);
+      targetsJson = [];
+    }
+
+    const alreadyExists = targetsJson.some(
+      (target) => target.labels?.hostname === hostname
+    );
+
+    if (!alreadyExists) {
+      targetsJson.push({
+        targets: [`${ip}:${port}`],
+        labels: { hostname: hostname },
+      });
+      await writeFile(targetsFilePath, JSON.stringify(targetsJson, null, 2));
+      fastify.log.info(`Added ${hostname} to Prometheus targets.`);
+    } else {
+      fastify.log.info(`${hostname} already exists in Prometheus. Skipping.`);
+    }
+  }
+}
+
+export default fp(prometheusPlugin);
